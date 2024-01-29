f1b6a80097c433ee4f9e694078cc4e41d0f7a02d
galinamoeed2024 <Galina.moeedny@gmail.com>
2024-01-29 15:19:23 -0500

    added an assertion

--- tests/galinamoeed24.spec.js
+++ tests/galinamoeed24.spec.js
@@ -3,6 +3,5 @@ const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
   await page.goto('https://www.nordstrom.com/');
-
-  
+  await expect(page).toHaveTitle('Nordstrom Online & In Store: Shoes, Jewelry, Clothing, Makeup, Dresses');  
 });
