3b15f2c8edb4be329c718557fe497f11702c21a7
galinamoeed2024 <Galina.moeedny@gmail.com>
2024-01-29 15:20:51 -0500

    commented out code for later

--- tests/visioniframeLearn.spec.js
+++ tests/visioniframeLearn.spec.js
@@ -3,10 +3,10 @@ const { test, expect}= require('@playwright/test');
 test('Verify user can locate iFrame within a page',async ({ page}) => {
 await page.goto('https://ui.vision/demo/webtest/frames/');
 await expect(page).toHaveTitle('Frames - Web Automation Test');
+//await page.locator('[name="mytext1"]').fill('iframe number one');
 
 
 
 
-1111
 
 });
