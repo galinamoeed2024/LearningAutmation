48301e6a377d3e5f331df27134f554d4388480f2
galinamoeed2024 <Galina.moeedny@gmail.com>
2024-01-30 16:36:19 -0500

    Login

--- tests/galinamoeed24.spec.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
-
-test('has title', async ({ page }) => {
-  await page.goto('https://www.nordstrom.com/');
-  await expect(page).toHaveTitle('Nordstrom Online & In Store: Shoes, Jewelry, Clothing, Makeup, Dresses');  
-});
