a92da7d98499cea354a4ba40466ac66543845ac8
galinamoeed2024 <Galina.moeedny@gmail.com>
2024-02-02 10:34:42 -0500

    broken link test

---
+++ tests/BrokenLinksTest.spec.js
@@ -0,0 +1,42 @@
+
+import { test, expect, request } from '@playwright/test';
+//const {test, expect} = require('@playwright/test');
+
+test('veryfy links on a page', async ({ page }) => {
+    await page.goto('https://insurancewebsitedemo.com/');
+    await page.locator('.get-a-quote').waitFor();
+
+    const linkElements = await page.$$('a');
+    console.log(`total links on the page are: ${linkElements.length}`);
+
+    let links = []
+
+    for (const linkElement of linkElements) {
+
+        if (linkElement) {
+            links.push(await linkElement.getAttribute('href'));
+
+        }
+    }
+    console.log(links.lenght);
+    for (const link of links) {
+        if (link && link.includes('http')) {
+            const res = await page.goto(link);
+            if (res) {
+                console.log(`${link} worked and the status is ${res.status()}`);
+
+            }
+            else {
+                console.log(`${link} did not work or retuen 200`);
+
+            }
+            await page.waitForTimeout(1000);
+        }
+
+
+
+
+    }
+
+
+});
